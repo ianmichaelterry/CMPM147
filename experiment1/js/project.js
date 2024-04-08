d185d58727c52fc2126137ff288d089d6e3418ee
Ian <ianmichaelterry@gmail.com>
2024-04-07 22:23:29 -0700

    add html

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,38 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+const fillers = {
+  adverb: ["probably", "really", "certainly", "arguably", "decidedly"],
+  verb: ["aggravate", "fornicate with", "commit arson with", "browse the dark web with", "murder", "defecate into the shoes of", "argue vehemetly with", "smoke salvia with", "defenestrate"],
+  subject: ["your mother", "the police", "your boss", "the Malaysian Prime Minister", "Mike Tyson", "your parole officer", "the district attorney"],
+};
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+const template = `Life can be complex, but can I see where you're coming from. You should $adverb $verb $subject.
+`;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  box.innerText = story;
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+clicker.onclick = generate;
+
+generate();
+main();
