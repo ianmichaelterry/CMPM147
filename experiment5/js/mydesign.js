23ae8b98b080dc3d0f448065ac4363e2ea80d708
Ian <ianmichaelterry@gmail.com>
2024-05-07 19:39:45 -0700

    revert to pre-color

--- experiment5/js/mydesign.js
+++ experiment5/js/mydesign.js
@@ -1,6 +1,3 @@
-/* exported p4_inspirations, p4_initialize, p4_render, p4_mutate */
-
-
 function getInspirations() {
   return [
     {
@@ -20,25 +17,6 @@ function getInspirations() {
     },
   ];
 }
-
-function weightedRandom(colors) {
-  let total = colors.reduce((sum, color) => sum + color.count, 0);
-  let threshold = Math.random() * total;
-  let cumulative = 0;
-
-  for (let color of colors) {
-    cumulative += color.count;
-    if (cumulative > threshold) {
-      return color.color;
-    }
-  }
-  return colors[colors.length - 1].color;
-}
-
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)].color;
-}
-
 
 function initDesign(inspiration) {
   // set the canvas size based on the container
@@ -65,42 +43,17 @@ function initDesign(inspiration) {
                     y: random(height),
                     w: random(width/2),
                     h: random(height/2),
-                    //fill: randomColor(inspiration.palette)})
-                    //fill: JSON.parse(JSON.stringify(randomColor(inspiration.palette)))})
                     fill: random(255)})
   }
   return design;
 }
-
-// function initDesign(inspiration) {
-//   let canvasWidth = $('.image-container').width();
-//   let aspectRatio = inspiration.image.height / inspiration.image.width;
-//   let canvasHeight = canvasWidth * aspectRatio;
-//   resizeCanvas(canvasWidth, canvasHeight);
-//   $(".caption").text(inspiration.credit);
-
-//   const imgHTML = `<img src="${inspiration.assetUrl}" style="width:${canvasWidth}px;">`
-//   $('#original').empty().append(imgHTML);
-
-//   let design = { bg: [128, 128, 128], fg: [] };
-//   for (let i = 0; i < 100; i++) {
-//     design.fg.push({
-//       x: random(width),
-//       y: random(height),
-//       w: random(width/2),
-//       h: random(height/2),
-//       fill: weightedRandom(inspiration.palette)
-//     });
-//   }
-//   return design;
-// }
 
 function renderDesign(design, inspiration) {
   
   background(design.bg);
   noStroke();
   for(let box of design.fg) {
-    fill(...box.fill);
+    fill(box.fill, 128);
     rect(box.x, box.y, box.w, box.h);
   }
 }
@@ -108,12 +61,7 @@ function renderDesign(design, inspiration) {
 function mutateDesign(design, inspiration, rate) {
   design.bg = mut(design.bg, 0, 255, rate);
   for(let box of design.fg) {
-    //box.fill = JSON.parse(JSON.stringify(weightedRandom(inspiration.palette)));
-    box.fill = JSON.parse(JSON.stringify(randomColor(inspiration.palette)));
-    //box.fill = mut(box.fill, 0, 255, rate);
-    if (Math.random() < rate / 100.0) {  // Convert rate to a probability
-       box.fill = JSON.parse(JSON.stringify(randomColor(inspiration.palette)));
-    }
+    box.fill = mut(box.fill, 0, 255, rate);
     box.x = mut(box.x, 0, width, rate);
     box.y = mut(box.y, 0, height, rate);
     box.w = mut(box.w, 0, width/2, rate);
--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -39,74 +39,6 @@ function inspirationChanged(nextInspiration) {
   setup();
 }
 
-// function getColorPalette(image, numColors) {
-//   let colorCounts = {};
-//   let sortedColors;
-//   image.loadPixels();
-//   for (let i = 0; i < image.pixels.length; i += 4) {
-//     let r = image.pixels[i];
-//     let g = image.pixels[i+1];
-//     let b = image.pixels[i+2];
-//     let alpha = image.pixels[i+3];
-//     if (alpha > 128) { // consider only opaque pixels
-//       let color = `${r},${g},${b}`;
-//       colorCounts[color] = (colorCounts[color] || 0) + 1;
-//     }
-//   }
-//   sortedColors = Object.keys(colorCounts).sort((a, b) => colorCounts[b] - colorCounts[a]);
-//   return sortedColors.slice(0, numColors).map(col => {
-//     let [r, g, b] = col.split(',');
-//     return {color: [r, g, b], count: colorCounts[col]};
-//   });
-// }
-
-function colorDistance(color1, color2) {
-  // Parse RGB values
-  let r1 = parseInt(color1[0]), g1 = parseInt(color1[1]), b1 = parseInt(color1[2]);
-  let r2 = parseInt(color2[0]), g2 = parseInt(color2[1]), b2 = parseInt(color2[2]);
-
-  // Euclidean distance between two colors
-  return Math.sqrt(Math.pow(r2 - r1, 2) + Math.pow(g2 - g1, 2) + Math.pow(b2 - b1, 2));
-}
-
-function getColorPalette(image, numColors) {
-  let colorCounts = {};
-  let distinctColors = [];
-  const similarityThreshold = 60; // Adjust this threshold based on your needs
-
-  image.loadPixels();
-  for (let i = 0; i < image.pixels.length; i += 4) {
-      let r = image.pixels[i];
-      let g = image.pixels[i + 1];
-      let b = image.pixels[i + 2];
-      let alpha = image.pixels[i + 3];
-      if (alpha > 128) { // consider only opaque pixels
-          let color = `${r},${g},${b}`;
-          colorCounts[color] = (colorCounts[color] || 0) + 1;
-      }
-  }
-
-  let sortedColors = Object.keys(colorCounts).sort((a, b) => colorCounts[b] - colorCounts[a]);
-
-  for (let color of sortedColors) {
-      let [r, g, b] = color.split(',');
-      let isNewColor = true;
-      for (let existingColor of distinctColors) {
-          if (colorDistance([r, g, b], existingColor.color) < similarityThreshold) {
-              isNewColor = false;
-              break;
-          }
-      }
-      if (isNewColor && distinctColors.length < numColors) {
-          distinctColors.push({color: [parseInt(r), parseInt(g), parseInt(b)], count: colorCounts[color]});
-      }
-  }
-  
-  return distinctColors;
-}
-
-
-
 function setup() {
   currentCanvas = createCanvas(width, height);
   currentCanvas.parent(document.getElementById("active"));
@@ -115,7 +47,6 @@ function setup() {
   bestDesign = currentDesign;
   image(currentInspiration.image, 0,0, width, height);
   loadPixels();
-  currentInspiration.palette = getColorPalette(currentInspiration.image, 15);
   currentInspirationPixels = pixels;
 }
 
