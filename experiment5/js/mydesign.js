6baa015a5cfbc66f08bba072518e4b9158aa3d16
Ian <ianmichaelterry@gmail.com>
2024-05-08 00:04:14 -0700

    cleaned up

--- experiment5/js/mydesign.js
+++ experiment5/js/mydesign.js
@@ -1,57 +1,22 @@
 function getInspirations() {
   return [
     {
-      name: "Man in a Suit Skateboarding through Central Park, 1965", 
+      name: "Man in a Suit Skateboarding through Central Park", 
       assetUrl: "img/skateboard_suit.jpg",
-      credit: "Lunch atop a Skyscraper, Charles Clyde Ebbets, 1932"
+      credit: "Life Magazine, 1965"
     },
     {
-      name: "Nightclubbing - Grace Jones, 1981", 
+      name: "Nightclubbing - Grace Jones", 
       assetUrl: "img/nightclubbing.jpeg",
-      credit: "Train Wreck At Monteparnasse, Levy & fils, 1895"
+      credit: "Grace Jones, 1981"
     },
     {
       name: "A checkerboard", 
       assetUrl: "img/checkerboard.jpg",
       credit: ""
     },
-    {
-      name: "Wallace Shawn, inconcievable", 
-      assetUrl: "img/wallace-shawn.jpg",
-      credit: "Migrant Mother near Nipomo, California, Dorothea Lange, 1936"
-    },
   ];
 }
-
-// function initDesign(inspiration) {
-//   // set the canvas size based on the container
-//   let canvasContainer = $('.image-container'); // Select the container using jQuery
-//   let canvasWidth = canvasContainer.width(); // Get the width of the container
-//   let aspectRatio = inspiration.image.height / inspiration.image.width;
-//   let canvasHeight = canvasWidth * aspectRatio; // Calculate the height based on the aspect ratio
-//   resizeCanvas(canvasWidth, canvasHeight);
-//   $(".caption").text(inspiration.credit); // Set the caption text
-
-//   // add the original image to #original
-//   const imgHTML = `<img src="${inspiration.assetUrl}" style="width:${canvasWidth}px;">`
-//   $('#original').empty();
-//   $('#original').append(imgHTML);
-
-  
-//   let design = {
-//     bg: 128,
-//     fg: []
-//   }
-  
-//   for(let i = 0; i < 700; i++) {
-//     design.fg.push({x: random(width),
-//                     y: random(height),
-//                     w: random(width/12),
-//                     h: random(height/12),
-//                     fill: random(255)})
-//   }
-//   return design;
-// }
 
 function initDesign(inspiration) {
   // Set the canvas size based on the container
@@ -73,7 +38,7 @@ function initDesign(inspiration) {
 
   // Use a switch statement to differentiate initialization based on the image name
   switch (inspiration.name) {
-    case "Man in a Suit Skateboarding through Central Park, 1965":
+    case "Man in a Suit Skateboarding through Central Park":
       // Process for this specific image
       for (let i = 0; i < 700; i++) {
         design.fg.push({
@@ -99,14 +64,14 @@ function initDesign(inspiration) {
       }
       break;
 
-    case "Nightclubbing - Grace Jones, 1981":
+    case "Nightclubbing - Grace Jones":
       // Process for this specific image
-      for (let i = 0; i < 4000; i++) {
+      for (let i = 0; i < 3000; i++) {
         design.fg.push({
           x: random(width),
           y: random(height),
-          w: random(width/60),
-          h: random(height/60),
+          w: random(width/4),
+          h: random(height/4),
           fill: random(255)
         });
       }
@@ -118,8 +83,8 @@ function initDesign(inspiration) {
         design.fg.push({
           x: random(width),
           y: random(height),
-          w: random(width/12),
-          h: random(height/12),
+          w: random(width/2),
+          h: random(height/2),
           fill: random(255)
         });
       }
@@ -130,22 +95,13 @@ function initDesign(inspiration) {
 }
 
 
-// function renderDesign(design, inspiration) {
-  
-//   background(design.bg);
-//   noStroke();
-//   for(let box of design.fg) {
-//     fill(box.fill, 128);
-//     rect(box.x, box.y, box.w, box.h);
-//   }
-// }
 
 function renderDesign(design, inspiration) {
 
 
   // Use a switch statement to differentiate rendering based on the inspiration name
   switch (inspiration.name) {
-    case "Man in a Suit Skateboarding through Central Park, 1965":
+    case "Man in a Suit Skateboarding through Central Park":
       background(design.bg);
       noStroke();
       // Specific rendering process for this image
@@ -161,29 +117,50 @@ function renderDesign(design, inspiration) {
       // Specific rendering process for this image
       for (let box of design.fg) {
         fill(box.fill, 128); // Different color settings or shapes could be used
-        rect(box.x, box.y, box.w, box.h); // Use ellipses for this image
+        rect(box.x, box.y, box.w, box.h); 
       }
       break;
 
-    case "Wallace Shawn, inconcievable":
+
+    case "Nightclubbing - Grace Jones":
       background(design.bg);
       noStroke();
-      // Specific rendering process for this image
+      // Render randomly rotated triangles
       for (let box of design.fg) {
-        fill(box.fill, 128); // You could modify color processing or shapes
-        triangle(box.x, box.y, box.x - box.w/2, box.y + box.h, box.x + box.w/2, box.y + box.h); // Example of using triangles
+        fill(box.fill, 128);
+
+        // Calculate the center of the triangle
+        let centerX = box.x + box.w / 2;
+        let centerY = box.y + box.h / 2;
+
+        // Apply rotation
+        push(); // Save the current drawing state
+        translate(centerX, centerY); // Move the origin to the center of the triangle
+        rotate(random(TWO_PI)); // Rotate by a random angle
+
+        // Draw the triangle around the new origin
+        triangle(-box.w / 2, box.h / 2, 0, -box.h / 2, box.w / 2, box.h / 2);
+
+        pop(); // Restore the original drawing state
       }
       break;
+
 
     default:
-      background(design.bg);
-      noStroke();
-      // Default rendering process if no specific instructions are given for an image
+
       for (let box of design.fg) {
-        fill(box.fill, 128); // Default color and transparency settings
-        rect(box.x, box.y, box.w, box.h); // Default shape is rectangle
-      }
-      break;
+        fill(box.fill, 128);
+        let centerX = box.x + box.w / 2;
+        let centerY = box.y + box.h / 2;
+
+        push(); // Save the current drawing state
+        translate(centerX, centerY); // Move the origin to the center of the ellipse
+        rotate(random(TWO_PI)); // Rotate by a random angle
+        ellipse(0, 0, box.w, box.h); // Draw the ellipse around the new origin
+        pop(); // Restore the original drawing state
+    }
+    break;
+  
   }
 }
 
